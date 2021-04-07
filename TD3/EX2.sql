fce4d9e6ffce6802097975e7cde7e90afb682f5b
azerpas <xskylix@hotmail.fr>
2021-04-07 13:52:22 +0200

    first commit

---
+++ TD3/EX2.sql
@@ -0,0 +1,20 @@
+-- 1.	Obtenir la liste des noms, prénom et date de naissance de tous les élèves.
+-- 2.	Obtenir la liste des spécialités des professeurs avec doublons puis sans doublons.
+-- 3.	Obtenir le nom et prénom des élèves pesant entre 40 et 60 kilos et inscrits en 1ère année ou des élèves inscrits en 2ème année.
+-- 4.	Obtenir le nom des élèves dont le poids est compris entre 60 et 80 kilos.
+-- 5.	Obtenir le nom des élèves dont le nom commence par L.
+-- 6.	Obtenir le nom des professeurs dont la spécialité est inconnue.
+-- 7.	Obtenir, pour chaque professeur, son nom et sa spécialité. Si cette dernière est inconnue, on souhaite afficher la chaîne de caractères : ‘****’.
+-- 8.	Obtenir les paires de noms de professeurs qui ont la même spécialité.
+-- 9.	Afficher l’âge moyen des élèves. Cet âge moyen sera exprimé en année.
+-- 10.	Obtenir pour chaque élève de 1ère année son nom et sa moyenne.
+-- 11.	Obtenir la moyenne des points de chaque élève de 1ère année dont le total des points est supérieur à 40.
+-- 12.	Obtenir le maximum parmi les totaux de chaque élève.
+-- 13.	Quels sont les élèves de 1ère année dont la moyenne est supérieure à la moyenne de la 1ère année ?
+-- 14.	Obtenir le nom et le poids des élèves de 1ère année plus lourds que n’importe quel élève de 2ème année.
+-- 15.	Obtenir le nom, le poids et l’année des élèves dont le poids est supérieur au poids moyen des élèves étant dans la même année d’études.
+-- 16.	Obtenir le nom des élèves de 1ère année qui ont obtenu plus de 60 % et qui jouent au tennis.
+-- 17.	Professeurs qui prennent en charge TOUS les cours de deuxième année ; on demande le Numéro et le nom.
+-- 18.	Elèves qui pratiquent TOUTES les activités ; on demande le Numéro et le nom.
+-- 19.	Quels sont les noms des élèves qui ne pratiquent aucune activité ? 
+-- 20.	Quels sont les noms d’activités pratiquées par tous les élèves ?
