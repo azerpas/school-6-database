3538d3108f274d13218a40f27df6b5122ee13fc6
azerpas <xskylix@hotmail.fr>
2021-04-14 15:13:52 +0200

    To 6.

--- TP1/2.sql
+++ TP1/2.sql
@@ -16,3 +16,23 @@ DELETE FROM Eleves_ValDeMarne WHERE Prenom ='Gil' AND Nom = 'JOURDAN';
 -- NON on ne peut pas supprimer Gil.
 -- Parce que la Vue est limitee aux eleves du 94. Hors Gil est du 93 donc non accessible depuis la vue
 
+-- 4.
+-- Peut-on ajouter un nouvel élève arrivant à l’ecole et venant de 94000 CRETEIL puis un autre
+-- élève venant de 91000 EVRY depuis la vue Eleves_ValDeMarne ? Pourquoi ? Verifier ?
+INSERT INTO eleves_valdemarne VALUES (11, 'Aime', 'Jean', '2004-12-12', 68, 2, 'm', 94000, 'CRETEIL');
+INSERT INTO eleves_valdemarne VALUES (12, 'Wallace', 'Mike', '2001-01-22', 55, 1, 'm', 91000, 'EVRY');
+
+-- OUI vu que la vue est monotable et renferme toutes les colonnes NOT NULL de la table Eleves
+
+-- 5.
+-- En utilisant la vue Eleves_ValDeMarne, donner les noms et prénoms des élèves du Val de
+-- Marne qui pratiquent du Tennis et qui ont une moyenne générale supérieure à 10.
+SELECT Eleves_ValDeMarne.NOM, PRENOM FROM eleves_valdemarne JOIN RESULTATS R on Eleves_ValDeMarne.NUM_ELEVE = R.NUM_ELEVE JOIN ACTIVITES_PRATIQUEES AP on Eleves_ValDeMarne.NUM_ELEVE = AP.NUM_ELEVE WHERE AP.NOM = 'Tennis' GROUP BY NUM_ELEVE HAVING AVG(R.POINTS) > 10;
+
+-- Correction:
+CREATE OR REPLACE VIEW VMoyennes AS SELECT Num_Eleve, Nom, Prenom, AVG(Points) AS Moyenne FROM Eleves_ValDeMarne NATURAL JOIN Resultats GROUP BY Num_Eleve, Nom, Prenom HAVING AVG(Points)>10;
+SELECT V.Nom, Prenom FROM VMoyennes V JOIN Activites_Pratiquees AP ON V.Num_Eleve = AP.Num_Eleve WHERE AP.Nom = 'Tennis';
+
+-- 6.
+CREATE USER User1 IDENTIFIED BY '123';
+grant select on Eleves To User1;
